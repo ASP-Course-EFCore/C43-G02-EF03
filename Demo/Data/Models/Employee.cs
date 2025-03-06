6039c2fd4bf8e150c7e7860060d49fb8973c87f4
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-03-06 14:21:01 +0200

    Part 05 Relationships Between Classes

---
+++ Demo/Data/Models/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Data.Models
+{
+    class Employee
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity/*Enum*/)]
+        public int Code { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50, MinimumLength = 10)]                    
+        [Length(10, 50)]
+        public string? Name { get; set; }
+
+        [Column(TypeName = "decimal(12,2)")]
+        [DataType(DataType.Currency)]
+        public double Salary { get; set; }
+
+        [Range(15, 35)]
+        [AllowedValues(20, 30, 40, 50)]
+        [DeniedValues(25, 35)]
+        public int? Age { get; set; }
+
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [RegularExpression("")]
+        public string? password { get; set; }
+
+        [Phone(ErrorMessage = "Must be 11 digits!")]
+        [DataType(DataType.PhoneNumber)]
+        public string? PhoneNumber { get; set; }
+
+        [NotMapped]
+        public double NetSalary { get { return Salary - (Salary * .2); } }
+
+
+        public double GetNetSalary => Salary - (Salary * .2);
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Demo
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 05 Relationships Between Classes
+
+            //How tho map relationship among tables (Entity Framework Core)?
+            // EF Core supports different types of relationships between tables, which are represented using =>
+            // (Navigation Properties - Foreign Key - Fluent APIs Configurations). 
+
+            //What is Navigational Property?
+            //Mean that you can navigate from one entity to another entity properties.
+            //It store the data.
+            //Navigational Property represent the relationship
+            //Navigational property in the class is of type the second class in relationship.
+
+            #endregion
         }
     }
 }
